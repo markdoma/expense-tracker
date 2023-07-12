43d10b37ed5872ede6cdc9d2f675225a5aa35e73
Mark Doma <markdoma10@gmail.com>
2023-07-12 14:04:25 +0800

    fix remaining avlues computation, add total card

--- components/BudgetComponent.js
+++ components/BudgetComponent.js
@@ -5,79 +5,120 @@ import { getFirestore } from "firebase/firestore";
 import { firestore } from "../utils/firebase";
 
 const BudgetComponent = () => {
-  const [budget, setBudget] = useState(0);
-  const [expenses, setExpenses] = useState([]);
-  const [remainingBudget, setRemainingBudget] = useState(0);
+  const [budget, setBudget] = useState({});
+  const [expenses, setExpenses] = useState({});
+  const [remainingBudget, setRemainingBudget] = useState({});
+  const [selectedMonthYear, setSelectedMonthYear] = useState("");
+  const [filteredTotal, setFilteredTotal] = useState(0);
 
   useEffect(() => {
-    // const firestore = getFirestore(); // Initialize your Firestore instance
-    // Fetch budget amount
     const unsubscribeBudget = firestore
       .collection("budget")
       .onSnapshot((snapshot) => {
+        const categoryBudget = {};
         snapshot.docs.forEach((doc) => {
           const budgetData = doc.data();
-          setBudget(budgetData.Budget);
+          const { Budget, Category } = budgetData;
+          categoryBudget[Category] = Budget;
         });
+        setBudget(categoryBudget);
       });
 
     const unsubscribeExpenses = firestore
       .collection("expenses")
       .onSnapshot((snapshot) => {
         const categoryTotals = {};
+        let total = 0;
         snapshot.docs.forEach((doc) => {
           const expenseData = doc.data();
-          const { amount, category } = expenseData;
+          const { amount, category, monthYear } = expenseData;
 
-          if (category in categoryTotals) {
-            categoryTotals[category] += amount;
-          } else {
-            categoryTotals[category] = amount;
+          if (!selectedMonthYear || monthYear === selectedMonthYear) {
+            if (category in categoryTotals) {
+              categoryTotals[category] += amount;
+            } else {
+              categoryTotals[category] = amount;
+            }
+
+            if (!selectedMonthYear) {
+              total += amount;
+            }
           }
         });
         setExpenses(categoryTotals);
+        setFilteredTotal(total);
       });
 
     return () => {
       unsubscribeBudget();
       unsubscribeExpenses();
     };
-  }, []);
+  }, [selectedMonthYear]);
 
   useEffect(() => {
     const calculateRemainingBudget = () => {
-      let totalExpenses = 0;
+      const remainingBudgetPerCategory = {};
 
-      Object.values(expenses).forEach((categoryTotal) => {
-        totalExpenses += categoryTotal;
+      Object.keys(budget).forEach((category) => {
+        const categoryTotal = expenses[category] || 0;
+        remainingBudgetPerCategory[category] = budget[category] - categoryTotal;
       });
 
-      setRemainingBudget(budget - totalExpenses);
+      setRemainingBudget(remainingBudgetPerCategory);
     };
 
     calculateRemainingBudget();
-  }, [expenses, budget]);
+  }, [budget, expenses]);
+
+  const handleMonthYearChange = (event) => {
+    setSelectedMonthYear(event.target.value);
+  };
 
   return (
     <div className="flex justify-center">
       <div className="overflow-x-auto">
+        <div className="mb-4">
+          <label className="mr-2">Filter by Month-Year:</label>
+          <select
+            className="border border-gray-300 rounded px-2 py-1"
+            value={selectedMonthYear}
+            onChange={handleMonthYearChange}
+          >
+            <option value="">All</option>
+            <option value="2023-01">January 2023</option>
+            <option value="2023-02">February 2023</option>
+            <option value="2023-03">March 2023</option>
+            {/* Add more options for each Month-Year */}
+          </select>
+        </div>
+        <div className="mb-4">
+          <div className="bg-gray-200 p-4 rounded-md">
+            <h3 className="text-lg font-semibold mb-2">
+              Total for Filtered Month-Year:
+            </h3>
+            <p className="text-2xl font-bold text-center">{filteredTotal}</p>
+          </div>
+        </div>
         <table className="table-auto">
           <thead>
             <tr>
               <th className="px-2 py-2">Category</th>
               <th className="px-2 py-2">Total</th>
-              {/* <th className="px-2 py-2">Status</th> */}
               <th className="px-2 py-2">Remaining</th>
             </tr>
           </thead>
           <tbody>
-            {Object.entries(expenses).map(([category, total]) => (
-              <tr key={category} className={total > budget ? "bg-red-200" : ""}>
+            {Object.keys(expenses).map((category) => (
+              <tr
+                key={category}
+                className={
+                  expenses[category] > budget[category] ? "bg-red-200" : ""
+                }
+              >
                 <td className="px-2 py-2 flex justify-center">{category}</td>
-                <td className="px-2 py-2">{total}</td>
-                {/* <td className="px-2 py-2 ">{total > budget ? "Over" : ""}</td> */}
+                <td className="px-2 py-2">{expenses[category] || 0}</td>
                 <td className="px-2 py-2 flex justify-center">
-                  {budget - total}
+                  {remainingBudget[category]}
                 </td>
               </tr>
             ))}
